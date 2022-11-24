9dd00405d6d276ede8ffdaea0eb636de95a4de3a
a.gribul <a.gribul@tinkoff.ru>
2022-11-24 17:33:27 +0300

    feat: Add WeightNormCallback

---
+++ TTS/callbacks/weights.py
@@ -0,0 +1,25 @@
+from trainer import Trainer
+
+
+class WeightNormCallback:
+
+    def __init__(self):
+        self._interval = 50
+
+    def __call__(self, trainer: Trainer, *args, **kwargs):
+        if trainer.total_steps_done % self._interval != 0:
+            return None
+
+        for name, parameters in trainer.model.named_parameters():
+            trainer.dashboard_logger.add_scalar(
+                title=f"grad_norms/{name}",
+                value=parameters.grad.data.norm(2).item(),
+                step=trainer.total_steps_done
+            )
+            trainer.dashboard_logger.add_scalar(
+                title=f"weight_norms/{name}",
+                value=parameters.data.norm(2).item(),
+                step=trainer.total_steps_done
+            )
+
+        return None
