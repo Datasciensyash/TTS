e1b276899e724d42e91a7a3dff1ab2cc7dd0da01
a.gribul <a.gribul@tinkoff.ru>
2022-11-24 19:33:19 +0300

    fix: Fix conformer (?)

--- TTS/tts/conformer/encoder.py
+++ TTS/tts/conformer/encoder.py
@@ -68,7 +68,7 @@ class ConformerEncoder(nn.Module):
 
         self._conformer_encoder = Encoder(
             idim=0,  # Unused, because input_layer=None
-            input_layer=None,
+            input_layer="linear",
             cnn_module_kernel=7,
             **params
         )
