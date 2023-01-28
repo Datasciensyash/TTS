b2c46e6ed733478d367ec67966003ddea1c215be
a.gribul <a.gribul@tinkoff.ru>
2023-01-28 13:17:31 +0300

    feat: Fix very stupid typo in WER

--- TTS/tts/models/vits.py
+++ TTS/tts/models/vits.py
@@ -1632,7 +1632,7 @@ class Vits(BaseTTS):
                 loader = DataLoader(
                     dataset,
                     batch_size=config.eval_batch_size if is_eval else config.batch_size,
-                    shuffle=False,  # shuffle is done in the dataset.
+                    shuffle=True,  # shuffle is done in the dataset.
                     collate_fn=dataset.collate_fn,
                     drop_last=False,  # setting this False might cause issues in AMP training.
                     num_workers=config.num_eval_loader_workers if is_eval else config.num_loader_workers,
