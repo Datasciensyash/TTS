79652471ad7f5c6875a47ef918ec54a428b5261c
a.gribul <a.gribul@tinkoff.ru>
2023-02-06 19:17:23 +0300

    feat: Univnet-VITS?

--- TTS/scripts/test_models.py
+++ TTS/scripts/test_models.py
@@ -61,88 +61,91 @@ def default_model_test(
     input_dir: Path,
     out_csv: Path,
 ) -> None:
+
+    wer_files = list((Path(__file__).parent / "script_data").glob("wer*.txt"))
+
+    wer_names = []
+    for i in wer_names:
+        wer_names.extend((i.stem, i.stem.replace("wer_", "cer_")))
+
+    columns = [
+        "model",
+        "mos_pred",
+        "noi_pred",
+        "dis_pred",
+        "col_pred",
+        "loud_pred",
+        "speaker_similarity",
+    ]
+    columns += wer_names
+
     with open(out_csv, "w", newline="") as csvfile:
         csvwriter = writer(csvfile)
-
-        wer_files = list((Path(__file__).parent / "script_data").glob("wer*.txt"))
-
-        wer_names = []
-        for i in wer_names:
-            wer_names.extend((i.stem, i.stem.replace("wer_", "cer_")))
-
-        columns = [
-            "model",
-            "mos_pred",
-            "noi_pred",
-            "dis_pred",
-            "col_pred",
-            "loud_pred",
-            "speaker_similarity",
-        ]
-        columns += wer_names
-
         csvwriter.writerow(columns)
 
-        for model_dir in model_dirs:
-            # Find all checkpoints in the model root directory
-            checkpoint_paths = [i for i in model_dir.rglob("*.pth") if i.stem != "speakers"]
+    for model_dir in model_dirs:
+        # Find all checkpoints in the model root directory
+        checkpoint_paths = [i for i in model_dir.rglob("*.pth") if i.stem != "speakers"]
 
-            for checkpoint_path in checkpoint_paths:
-                checkpoint = checkpoint_path.name
-                model_root_dir = checkpoint_path.parent
+        for checkpoint_path in checkpoint_paths:
+            checkpoint = checkpoint_path.name
+            model_root_dir = checkpoint_path.parent
 
-                sentinel = (model_root_dir / checkpoint).with_suffix(".test.json")
-                if sentinel.exists():
-                    continue
-                # Run MOS testing
-                # NOTE: compute_mos_nisqa returns Tuple[float, float, float, float, float]
-                # NOTE: compute_mos_nisqa returns Tuple of "mos_pred", "noi_pred", "dis_pred", "col_pred", "loud_pred"
-                mos_output = compute_mos_nisqa(
+            sentinel = (model_root_dir / checkpoint).with_suffix(".test.json")
+            if sentinel.exists():
+                continue
+            # Run MOS testing
+            # NOTE: compute_mos_nisqa returns Tuple[float, float, float, float, float]
+            # NOTE: compute_mos_nisqa returns Tuple of "mos_pred", "noi_pred", "dis_pred", "col_pred", "loud_pred"
+            mos_output = compute_mos_nisqa(
+                input_dir=input_dir,
+                model_root_dir=model_root_dir,
+                speaker_encoder_checkpoint_path=speaker_encoder_checkpoint_path,
+                checkpoint_name=checkpoint,
+                device=device,
+            )
+            mos_pred, noi_pred, dis_pred, col_pred, loud_pred = mos_output
+
+            # Run speaker similarity testing
+            speaker_similarity = compute_speaker_similarity(
+                input_dir=input_dir,
+                device=device,
+                model_root_dir=model_root_dir,
+                speaker_encoder_checkpoint_path=speaker_encoder_checkpoint_path,
+                checkpoint_name=checkpoint,
+            )
+            data_row = [
+                str(model_root_dir / checkpoint),
+                mos_pred,
+                noi_pred,
+                dis_pred,
+                col_pred,
+                loud_pred,
+                speaker_similarity,
+            ]
+
+            # Run WER testing
+            for file in wer_files:
+                wer, cer = compute_wer(
                     input_dir=input_dir,
                     model_root_dir=model_root_dir,
                     speaker_encoder_checkpoint_path=speaker_encoder_checkpoint_path,
                     checkpoint_name=checkpoint,
                     device=device,
+                    texts_file=file,
+                    max_speakers_num=2,
                 )
-                mos_pred, noi_pred, dis_pred, col_pred, loud_pred = mos_output
+                data_row.append(wer)
+                data_row.append(cer)
 
-                # Run speaker similarity testing
-                speaker_similarity = compute_speaker_similarity(
-                    input_dir=input_dir,
-                    device=device,
-                    model_root_dir=model_root_dir,
-                    speaker_encoder_checkpoint_path=speaker_encoder_checkpoint_path,
-                    checkpoint_name=checkpoint,
-                )
-                data_row = [
-                    str(model_root_dir / checkpoint),
-                    mos_pred,
-                    noi_pred,
-                    dis_pred,
-                    col_pred,
-                    loud_pred,
-                    speaker_similarity,
-                ]
-
-                # Run WER testing
-                for file in wer_files:
-                    wer, cer = compute_wer(
-                        input_dir=input_dir,
-                        model_root_dir=model_root_dir,
-                        speaker_encoder_checkpoint_path=speaker_encoder_checkpoint_path,
-                        checkpoint_name=checkpoint,
-                        device=device,
-                        texts_file=file,
-                        max_speakers_num=2,
-                    )
-                    data_row.append(wer)
-                    data_row.append(cer)
-
+            with open(out_csv, "a", newline="") as csvfile:
+                csvwriter = writer(csvfile)
                 csvwriter.writerow(data_row)
-                with sentinel.open("w") as f:
-                    json.dump(dict(zip(columns, data_row)), f)
 
-        print(f"Test results are saved in {out_csv}")
+            with sentinel.open("w") as f:
+                json.dump(dict(zip(columns, data_row)), f)
+
+    print(f"Test results are saved in {out_csv}")
 
 
 if __name__ == "__main__":
