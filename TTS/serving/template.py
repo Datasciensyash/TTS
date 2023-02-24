63c68258f3a1092b2b82c0a2b1e4a651a5661861
a.gribul <a.gribul@tinkoff.ru>
2023-02-24 20:38:02 +0300

    feat: Add serving for speaker encoder

--- TTS/serving/template.py
+++ TTS/serving/template.py
@@ -16,7 +16,7 @@ def copy_template_config(copy_path: Path) -> None:
 
 def copy_template_config_se(copy_path: Path) -> None:
     config = SE_MODEL_CONFIG_TEMPLATE_PATH.read_text()
-    config = config.replace("MODEL_NAME", MODEL_SE_NAME)
+    config = config.replace("MODEL_SE_NAME", MODEL_SE_NAME)
     config = config.replace("AUDIO_SIGNAL_INPUT_NAME", AUDIO_SIGNAL_INPUT_NAME)
     config = config.replace("AUDIO_SIGNAL_SAMPLING_RATE", AUDIO_SIGNAL_SAMPLING_RATE)
     config = config.replace("SPEAKER_EMBEDDING_OUTPUT_NAME", SPEAKER_EMBEDDING_OUTPUT_NAME)
