2e2a8f627f9fc9e7730c3460b15aa25771d9038b
a.gribul <a.gribul@tinkoff.ru>
2023-02-24 19:14:57 +0300

    feat: Add serving for speaker encoder

--- TTS/serving/template.py
+++ TTS/serving/template.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
-from TTS.serving.constants import TEXT_INPUT_NAME, SPEAKER_EMBEDDING_INPUT_NAME, \
-    AUDIO_SIGNAL_OUTPUT_NAME, TTS_MODEL_CONFIG_TEMPLATE_PATH, MODEL_NAME
+from TTS.serving.constants import *
 
 
 def copy_template_config(copy_path: Path) -> None:
@@ -16,11 +15,9 @@ def copy_template_config(copy_path: Path) -> None:
 
 
 def copy_template_config_se(copy_path: Path) -> None:
-    config = TTS_MODEL_CONFIG_TEMPLATE_PATH.read_text()
-    config = config.replace("MODEL_NAME", MODEL_NAME)
-    config = config.replace("TEXT_INPUT_NAME", TEXT_INPUT_NAME)
-    config = config.replace("SPEAKER_EMBEDDING_INPUT_NAME", SPEAKER_EMBEDDING_INPUT_NAME)
-    config = config.replace(
-        "AUDIO_SIGNAL_OUTPUT_NAME", AUDIO_SIGNAL_OUTPUT_NAME
-    )
+    config = SE_MODEL_CONFIG_TEMPLATE_PATH.read_text()
+    config = config.replace("MODEL_NAME", MODEL_SE_NAME)
+    config = config.replace("AUDIO_SIGNAL_INPUT_NAME", AUDIO_SIGNAL_INPUT_NAME)
+    config = config.replace("AUDIO_SIGNAL_SAMPLING_RATE", AUDIO_SIGNAL_SAMPLING_RATE)
+    config = config.replace("SPEAKER_EMBEDDING_OUTPUT_NAME", SPEAKER_EMBEDDING_OUTPUT_NAME)
     copy_path.write_text(config)
